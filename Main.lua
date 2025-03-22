9c3c1c1fae55247f0881364960924d0cfb4c954e
0XxVITORxX0 <luizluau2.0@gmail.com>
2025-03-22 13:06:44 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2632,7 +2632,7 @@ v23.BorderSizePixel = 0;
 v23.Position = UDim2.new(0.120833337 - 0.1, 0, 0.0952890813 + 0.01, 0);
 v23.Size = UDim2.new(0, 50, 0, 50);
 v23.Draggable = true;
-v23.Image = "http://www.roblox.com/asset/?id=103985507475473";
+v23.Image = "http://www.roblox.com/asset/?id=85630046695458";
 v24.Parent = v23;
 v24.CornerRadius = UDim.new(0, 12);
 v25.Parent = v23;
